639e3e9ed8e2fb65655b344d21e7eb730c039819
dongoa <ao.dong@siat.ac.cn>
2019-04-30 17:40:46 +0800

    hei

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ attributeF([[],[],[]],paper_data,paper_data,-1);
 var hei=$('.item1').height();
 console.log(hei);
 var div=document.getElementById('');
-$('.venues-body').css('height',hei*0.1);
-$('.author-body').css('height',hei*0.33);
-$('.keyword-body').css('height',hei*0.35);
+$('.venues-body').css('height',hei*0.04);
+$('.author-body').css('height',hei*0.28);
+$('.keyword-body').css('height',hei*0.3);
 
 $('.venues-head').css('height',hei*0.04);
 $('.author-head').css('height',hei*0.04);
