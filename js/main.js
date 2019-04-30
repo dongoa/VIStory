92e58712010828553ba88359b1f7a3ef6c797c00
dongoa <ao.dong@siat.ac.cn>
2019-04-30 17:02:27 +0800

    hei

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 import {attributeF} from './attributeF.js';
 var paper_data=PAPER;
 attributeF([[],[],[]],paper_data,paper_data,-1);
+var hei=$('.item1').height();
+console.log(hei);
+var div=document.getElementById('');
+$('.venues-body').css('height',hei*0.2);
+$('.author-body').css('height',hei*0.3);
+$('.keyword-body').css('height',hei*0.3);
